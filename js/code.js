87f3e878fff628e6d1db710171bba4cd201411b5
Daniel Lewis <daniellewis777@gmail.com>
2023-11-07 16:12:24 +0000

    reload-playground

---
+++ js/code.js
@@ -0,0 +1,7 @@
+const origColor = 'rgb(2, 115, 206)'
+const doIt = () => {
+  const el = document.querySelector('button')
+  const style = window.getComputedStyle(el)
+  const bgColor = style.backgroundColor
+  el.style.backgroundColor = bgColor === origColor ? '#000000' : origColor
+}
