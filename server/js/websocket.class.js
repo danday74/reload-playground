79e35760299bfba5d661f991238ce8909f63669c
Daniel Lewis <daniellewis777@gmail.com>
2023-11-10 02:41:34 +0000

    logging

--- server/js/websocket.class.js
+++ server/js/websocket.class.js
@@ -4,13 +4,22 @@ const defer = require('./defer')
 
 class Websocket {
   #ws = null
-  #messageFunc = data => console.log(`ws receive ${config.bullet}${config.bullet}`, data)
+  #messageFunc = data => this.#log('message', data)
+
+  #log(name, value) {
+    if (value) {
+      console.log(`WS ${name} ${config.bullet}${config.bullet}`, value)
+    } else {
+      console.log(`WS ${name}`)
+    }
+  }
 
   async init(port = config.websocketPort) {
     const wss = new WebSocketServer({ port })
     const deferred = defer()
 
     wss.on('connection', (ws) => {
+      this.#log('connection')
       this.#ws = ws
       this.#ws.on('error', console.error)
       this.#ws.on('message', rawData => {
@@ -30,10 +39,10 @@ class Websocket {
 
   send(name, payload = null) {
     if (this.#ws == null || this.#ws.readyState !== 1) {
-      console.warn('ws not open')
+      this.#log('not open')
     } else {
       const obj = { name, payload }
-      console.log(`ws send ${config.bullet}${config.bullet}`, obj)
+      this.#log('send', obj)
       this.#ws.send(JSON.stringify(obj))
     }
   }
